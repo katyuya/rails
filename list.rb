063d192366ba7428606ddd51fcc8fd6a489140f8
EC2 Default User <ec2-user@ip-172-31-4-139.ec2.internal>
2021-11-14 15:58:04 +0000

    [add]

--- list.rb
+++ list.rb
@@ -1,10 +1,18 @@
 class List < ApplicationRecord
+  attachment :image
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
   def change
     create_table :lists do |t|
       t.string :title
       t.string :body
 
       t.timestamps
+
+
     end
   end
 end
+
